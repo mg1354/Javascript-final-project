68b630a948410b966125cb571b22333a5d3fe9c4
Mohammad GilaniLarimi <70506065+mg1354@users.noreply.github.com>
2020-12-21 23:10:12 +0100

    finish
    I solved the problems and finished coding.

--- notePad.js
+++ notePad.js
@@ -1,26 +1,76 @@
 
-// function Note(date,title,note){
-//     this.date=date;
-//     this.title=title;
-//     this.note=note;
-//     this.titlelist=function(){
-//         return this.title+"\n"+"-------------"
-//     }
-// }
-
-//  var notes=[];
+function Note(date,title,note){
+    this.date= date;
+    this.title= title;
+    this.note= note;
+}
 
 
+init();
 
 function save(){
-    var title= document.getElementById("title").value;
-    console.log(title);
-    var li = document.createElement("li");
-        li.innerHTML = title;  
-        document.getElementById("ul").appendChild(li);
-}        
+     const date = new Date();
+     const title = document.getElementById("title").value;
+     const note = document.getElementById("note").value;
+     const theNote = new Note(date, title, note);
+     saveNotes(theNote);
+     document.getElementById("title").value='';
+     document.getElementById("note").value='';
+     init();
+ }
 
-var li= getElementsByTag("li");
 
+function saveNotes(note){
     
+    let noteArray= JSON.parse(localStorage.getItem('note'));
+    if(noteArray ==null ){
+        noteArray=new Array();
+        noteArray.push(note);
+    } else {
+        noteArray.push(note);
+    }
+    localStorage.setItem('note', JSON.stringify(noteArray));
+    
+}    
+    
+
+function init(){
+    
+    document.getElementById("container").innerHTML="";
+    let noteArray= JSON.parse(localStorage.getItem('note'));
+    if(noteArray !=null && noteArray !=""){
         
+        for(let x=0; x<noteArray.length; x++){
+         
+        var box = document.createElement("div");
+        box.setAttribute("class", "box");
+
+        var notedate=document.createElement("h5");
+        notedate.innerHTML = "Date: " + noteArray[x].date ;  
+        
+        var subject = document.createElement("h4");
+        subject.innerHTML = "title: " + noteArray[x].title ;    
+        
+        var body= document.createElement("p");
+        body.innerHTML = "note:"+noteArray[x].note;
+        
+        var remove = document.createElement("button");
+        remove.innerHTML= "Delete";
+        remove.setAttribute("id","remove");
+        
+        box.appendChild(notedate);
+        box.appendChild(subject);
+        box.appendChild(body);
+        box.appendChild(remove);
+        document.getElementById("container").appendChild(box);
+        remove.addEventListener("click", function(){
+        noteArray.splice(x,1);
+        localStorage.setItem('note',JSON.stringify(noteArray));
+        init();
+        });
+ 
+           
+      }
+    }
+}
+
